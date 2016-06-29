8b49180724252eb25e099e9c5ef8e26d00ffad4a
Minwei Shen <minwei@conviva.com>
2016-06-28 22:08:08 -0700

    take screenshot functionality

--- detector.js
+++ detector.js
@@ -11,7 +11,8 @@ var gradient2 = ctxVolume.createLinearGradient(0,0,800,0);
 gradient2.addColorStop(0,"green");
 gradient2.addColorStop(1,"white");
 ctxVolume.fillStyle = gradient2;
-
+var ctxStage = document.querySelector("#hidden-stage").getContext('2d');
+var screenshot = document.querySelector("#screenshot")
 var video = document.querySelector('video');
 var addAudio = document.querySelector("#audio_file");
 var audioPlayer = document.querySelector("#audio_player");
@@ -24,6 +25,12 @@ var frequencyBuff, analyser;
 var isPlaying = false;
 var audioFiles;
 
+
+function takePhoto(){
+    ctxStage.drawImage(video, 0, 0, 400, 300);
+    var data = document.querySelector("#hidden-stage").toDataURL('image/png');
+    screenshot.setAttribute('src', data);
+}
 
 function reachFrequency(level){
     for(var i=level;i < frequencyBuff.length;i++){
@@ -64,6 +71,7 @@ function analyze() {
 
     // see if need to trigger handler
     if(reachThreshold(average, frequencyThresholdValue)){
+        takePhoto();
         var cnt = audioFiles.length;
         var vendorURL = window.URL || window.webkitURL;
         audioPlayer.src = vendorURL.createObjectURL(audioFiles[Math.floor(Math.random()*cnt)]);
